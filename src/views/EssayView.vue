2bef885f153e201b1bdc984b8e855d8ba18080bb
Reetik Patel <reetikp@gmail.com>
2025-10-26 18:07:03 -0500

    ✨ finished essay page

--- src/views/EssayView.vue
+++ src/views/EssayView.vue
@@ -5,9 +5,136 @@ import TiltCard from "@/components/TiltCard.vue";
 <template>
   <div class="flex gap-4 items-center justify-evenly w-full">
     <div class="flex flex-col gap-4">
-      <h1>Essay</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      <p
+        class="max-w-prose leading-relaxed text-base indent-16 overflow-y-auto max-h-[calc(2/3*100vh)] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+        error sit voluptatem accusantium doloremque laudantium, totam rem
+        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+        dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+        sed quia non numquam eius modi tempora incidunt ut labore et dolore
+        magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+        aliquid ex ea commodi consequatur. Vivamus luctus urna sed urna
+        ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed
+        nec diam eu diam mattis viverra. Fusce condimentum lectus vitae quam
+        cursus, at bibendum est gravida. Morbi libero risus, imperdiet in
+        facilisis eget, sodales at nulla. Aenean in arcu tristique, fermentum
+        est vitae, rutrum est. Cras vitae condimentum arcu. Mauris porttitor
+        elit a elit viverra, vitae pellentesque magna molestie. Aenean vitae
+        enim magna. Fusce condimentum lectus vitae quam cursus, at bibendum est
+        gravida. Morbi libero risus, imperdiet in facilisis eget, sodales at
+        nulla. Aenean in arcu tristique, fermentum est vitae, rutrum est. Cras
+        vitae condimentum arcu. Mauris porttitor elit a elit viverra, vitae
+        pellentesque magna molestie. Aenean vitae enim magna. Pellentesque
+        habitant morbi tristique senectus et netus et malesuada fames ac turpis
+        egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor
+        sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean
+        ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet
+        est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum
+        sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget
+        tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus
+        enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
+        Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue,
+        eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi,
+        tincidunt quis, accumsan porttitor, facilisis luctus, metus. Phasellus
+        ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula
+        vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a,
+        sodales sit amet, nisi. Pellentesque fermentum dolor. Aliquam quam
+        lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc. Sed
+        adipiscing nulla sed imperdiet commodo. Phasellus volutpat, metus eget
+        egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut
+        lacus. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus
+        orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis
+        orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus,
+        bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede
+        sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean
+        posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu
+        sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec,
+        volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar,
+        augue ac venenatis condimentum, sem libero volutpat nibh, nec
+        pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus.
+        Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis
+        diam. Pellentesque ut neque. Pellentesque habitant morbi tristique
+        senectus et netus et malesuada fames ac turpis egestas. Proin imperdiet,
+        enim id ornare imperdiet, quam neque iaculis mi, a pharetra dolor enim
+        aliquet purus. Aenean iaculis. Aenean leo ligula, porttitor eu,
+        consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in,
+        viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
+        laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel
+        augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam
+        rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam
+        semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc,
+        blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et
+        ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
+        Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis
+        leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.
+        Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis
+        gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum
+        purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam
+        accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium
+        turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec,
+        imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante
+        arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent
+        adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus.
+        Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl
+        sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id,
+        imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus.
+        Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet
+        imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor
+        et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non,
+        euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent
+        congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere
+        vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed
+        aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat
+        dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+        voluptatibus explicabo nulla cum. Eum necessitatibus alias asperiores,
+        fugit aliquam doloribus odio voluptas minus voluptatum repellendus optio
+        omnis rem, deleniti repellat quas. Perspiciatis laudantium quasi minus.
+        Neque nihil voluptatem tempore repellat error, consequuntur harum optio
+        ipsum voluptatibus quae quam est, dolore expedita odio tenetur rem
+        aspernatur commodi ducimus quidem architecto reprehenderit voluptas ad
+        inventore officiis. Dolores velit odio autem beatae sit obcaecati
+        voluptas nihil ducimus minima quo commodi doloribus aperiam optio,
+        veritatis delectus deleniti officiis rerum repellendus quod molestias
+        voluptate nulla laboriosam temporibus iste molestiae officia. Officiis
+        fuga labore consectetur, autem eaque provident omnis praesentium
+        voluptas voluptatibus accusantium, enim quasi deserunt culpa tempora
+        nihil pariatur commodi ratione error nulla repellendus asperiores
+        distinctio quam repudiandae. Quis dolorem libero earum pariatur numquam
+        veritatis, officiis non modi at cum autem quae ratione aliquam nemo
+        nulla aspernatur exercitationem quibusdam cumque mollitia culpa
+        laudantium nesciunt commodi tempore atque dolor repudiandae voluptatibus
+        accusantium magnam deserunt voluptatum molestias amet recusandae
+        doloribus. Hic ad tempora nulla, exercitationem consequatur excepturi
+        nesciunt perferendis consectetur optio iusto dignissimos alias deserunt
+        inventore praesentium ipsum quos blanditiis at adipisci ullam numquam
+        fugiat doloribus accusantium molestiae quod nihil provident architecto
+        quae harum voluptas reprehenderit voluptatibus repellendus recusandae
+        necessitatibus ipsa. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus
+        error sit voluptatem accusantium doloremque laudantium, totam rem
+        aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+        voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni
+        dolores eos qui ratione voluptatem sequi nesciunt.
       </p>
     </div>
     <TiltCard />
