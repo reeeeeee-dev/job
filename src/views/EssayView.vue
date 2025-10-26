6457204c4161018aa8eeb32d3b9d40466bb20d1e
Reetik Patel <reetikp@gmail.com>
2025-10-26 17:55:47 -0500

    💄 fixed essay layout

--- src/views/EssayView.vue
+++ src/views/EssayView.vue
@@ -3,7 +3,7 @@ import TiltCard from "@/components/TiltCard.vue";
 </script>
 
 <template>
-  <div class="flex gap-4 items-center justify-between w-full">
+  <div class="flex gap-4 items-center justify-evenly w-full">
     <div class="flex flex-col gap-4">
       <h1>Essay</h1>
       <p>
