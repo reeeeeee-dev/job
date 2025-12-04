35649c9610de3710d83e0badedd170813a129ba4
Reetik Patel <reetikp@gmail.com>
2025-12-03 20:53:57 -0600

    ➕ vite-svg-loader

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import svgLoader from "vite-svg-loader";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
@@ -10,7 +11,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), cloudflare(), tailwindcss(), svgLoader()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
